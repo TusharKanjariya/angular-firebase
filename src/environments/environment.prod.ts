1c297eaf39f3cbdf029c57aeba1c6101783000dd
TusharKanjariya <kanjariyaram2017@gmail.com>
2020-02-26 11:58:12 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebaseConf: {
-    apiKey: "AIzaSyC_8cua2MsQdP9zug9Y7CVM7nzI_q5MNcw",
-    authDomain: "design-6ba50.firebaseapp.com",
-    databaseURL: "https://design-6ba50.firebaseio.com",
-    projectId: "design-6ba50",
-    storageBucket: "design-6ba50.appspot.com",
-    messagingSenderId: "60300125166",
-    appId: "1:60300125166:web:2e4caac3051ae30f61a4ff",
-    measurementId: "G-ZKM6T9M84G"
+    apiKey: "<YOUR_API_KEY>",
+    authDomain: "<YOUR_AUTH_DOMAIN>",
+    databaseURL: "<YOUR_DB_URL>",
+    projectId: "<YOUR_PROJECT_ID>",
+    storageBucket: "<YOUR_STORGAE_BUK>",
+    messagingSenderId: "<YOUR_ID>",
+    appId: "<YOUR_APP_ID>",
+    measurementId: "<YOUR_MEASURE_ID>"
   }
 };
